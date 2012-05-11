4a0ff7f9338582f89ee1022826848566be9fc764
Mitsutomo Kasama <m.kasama@everyleaf.com>
2012-05-11 16:50:26 +0900

    add myLocationOverlay

--- src/com/example/android/MapViewSampleActivity.java
+++ src/com/example/android/MapViewSampleActivity.java
@@ -2,21 +2,37 @@ package com.example.android;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
 
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MapViewSampleActivity extends MapActivity {
+
+	private MapView mapView;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		MapView mapView = (MapView) findViewById(R.id.map);
-		mapView.setClickable(true);
+		mapView = (MapView) findViewById(R.id.map);
 		mapView.setBuiltInZoomControls(true);
 
+		final MyLocationOverlay overlay = new MyLocationOverlay(
+				getApplicationContext(), mapView);
+		overlay.onProviderEnabled(LocationManager.GPS_PROVIDER); // GPS を使用する
+		overlay.enableMyLocation();
+		overlay.runOnFirstFix(new Runnable() {
+
+			public void run() {
+				mapView.getController().animateTo(overlay.getMyLocation()); // 現在位置を自動追尾する
+			}
+		});
+		mapView.getOverlays().add(overlay);
 		mapView.invalidate();
+		mapView.getController().setZoom(17); // 1～21の値をセット
 	}
 
 	@Override
