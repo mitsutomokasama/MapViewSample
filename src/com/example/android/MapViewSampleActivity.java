00dceb0907609ba7e109a35ebdeefe21e16daf77
Mitsutomo Kasama <m.kasama@everyleaf.com>
2012-05-11 14:50:52 +0900

    add overlay

--- src/com/example/android/MapViewSampleActivity.java
+++ src/com/example/android/MapViewSampleActivity.java
@@ -1,8 +1,16 @@
 package com.example.android;
 
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.Projection;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
 
 public class MapViewSampleActivity extends MapActivity {
@@ -16,11 +24,46 @@ public class MapViewSampleActivity extends MapActivity {
 		mapView.setClickable(true);
 		mapView.setBuiltInZoomControls(true);
 
+		MyOverlay myOverlay = new MyOverlay();
+		List<Overlay> overlayList = mapView.getOverlays();
+		overlayList.add(myOverlay);
+
 		mapView.invalidate();
 	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
+	}
+
+	private class MyOverlay extends Overlay {
+		private GeoPoint mGeoPoint;
+		public MyOverlay() {
+			mGeoPoint = null;
+		}
+
+		@Override
+		public boolean onTap(GeoPoint p, MapView mapView) {
+			mGeoPoint = p;
+			return super.onTap(p, mapView);
+		}
+
+		@Override
+		public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+			super.draw(canvas, mapView, shadow);
+			if (!shadow) {
+				if (mGeoPoint != null) {
+					Projection projection = mapView.getProjection();
+		            Point point = new Point();
+		            projection.toPixels(mGeoPoint, point);
+
+					Paint paint = new Paint();
+					paint.setStyle(Paint.Style.FILL);
+					paint.setARGB(255, 255, 0, 0);
+
+					canvas.drawCircle(point.x, point.y, 16, paint);
+				}
+			}
+		}
 	}
 }
